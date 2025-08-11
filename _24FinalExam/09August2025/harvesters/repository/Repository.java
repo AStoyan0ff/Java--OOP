ec8e96ab1bb8db397338366e4496cf32b397535d
AStoyanoff <chessterfield@gmail.com>
2025-08-12 00:12:55 +0300

    Update Repository.java

---
+++ _24FinalExam/09August2025/harvesters/repository/Repository.java
@@ -0,0 +1,9 @@
+package harvesters.repository;
+import java.util.Collection;
+
+public interface Repository<T> {
+
+    Collection<T> getCollection();
+    void add(T entity);
+    T byName(String name);
+}
